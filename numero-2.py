1c42de20f44006fa67140ac936a2b40b199dd732
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2025-04-03 20:11:33 -0300

    Add files via upload

---
+++ numero-2.py
@@ -0,0 +1,3 @@
+numero = input("Informe um número: ")
+
+print(f"O número informado foi {numero}.")
---
+++ soma-3.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Informe o primeiro número: "))
+
+numero2 = float(input("Informe o segundo número: "))
+
+soma = numero1 + numero2
+
+print(f"A soma dos números é: {soma}")
