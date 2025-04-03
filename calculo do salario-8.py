50401cf0acd7912e2960d9cdcddda6a4e763ba6a
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2025-04-03 19:55:44 -0300

    Add files via upload

---
+++ calculo do salario-8.py
@@ -0,0 +1,9 @@
+
+salario_por_hora = float(input("Quanto você ganha por hora? R$: "))
+horas_trabalhadas = float(input("Quantas horas você trabalhou no mês? "))
+
+
+salario_mensal = salario_por_hora * horas_trabalhadas
+
+
+print(f"O total do seu salário no mês é: R$ {salario_mensal:.2f}")
---
+++ temperatura-9.py
@@ -0,0 +1,5 @@
+temperatura_fahrenheit = float(input("Informe a temperatura em graus Fahrenheit: "))
+
+temperatura_celsius = (temperatura_fahrenheit - 32) * 5/9
+
+print(f"A temperatura em graus Celsius é: {temperatura_celsius:.2f}°C")
